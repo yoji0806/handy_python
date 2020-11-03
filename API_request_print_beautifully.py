9f689decec2161547105ab14494629a7e7c05530
Yamamoto Yoji <yamamoto@allegrosmart.com>
2020-11-03 12:39:41 +0900

    [update] add error handler of http status_code

--- API_request_print_beautifully.py
+++ API_request_print_beautifully.py
@@ -10,7 +10,14 @@ headers = {'Authorization' : 'Basic xxxx'}
 # asusume resonse.text is <str> type
 response = requests.request("GET", url, headers=headers, data = payload)
 
-response_json = json.loads(response.text)
+status_code = response.status_code
 
-# if response includes Japanese characters, put "ensure_ascii=False"
-print(json.dumps(response_json, indent=4, ensure_ascii=False))
+status_code = response.status_code
+
+if(status_code == 200):
+    response_json = json.loads(response.text)
+    # if response includes Japanese characters, put "ensure_ascii=False"
+    print(json.dumps(response_json, indent=4, ensure_ascii=False))
+else:
+    print("error: status code: ", status_code)
+
